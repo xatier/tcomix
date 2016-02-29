38c87d22e0ea44328c3c677b529978775a936090
xatier <xatierlike@gmail.com>
2016-03-01 07:04:25 +0800

    adopt size adjustment approach from ranger
    add a box

--- tcomix.py
+++ tcomix.py
@@ -34,26 +34,35 @@ class Tcomix(object):
         self.image_list = self.imgfoo().split()
         self.image_idx = 0
 
-        self.MAXW = 1280
-        self.MAXH = 720
-
         try:
             # initialize curses stuff
             self.win = curses.initscr()
             curses.noecho()
             curses.curs_set(False)
             self.win.keypad(True)
+
             self.win.erase()
             self.win.border()
-            self.win.refresh()
+
+            self.filelist_box_width = 20
+            self.filelist_box = curses.newwin(curses.LINES,
+                                              self.filelist_box_width,
+                                              0, 0)
+            self.filelist_box.border()
 
             self.my, self.mx = curses.LINES, curses.COLS
             self.cury, self.curx = self.my-2, 1
 
+            self.fontw, self.fonth = self.get_font_dimensions()
+
         except:
             print("GG on python curses init!")
 
+        self.MAXW = self.get_screen_size()[0] - self.fontw*23
+        self.MAXH = self.get_screen_size()[1] - self.fonth*2
+
         self.win.refresh()
+        self.filelist_box.refresh()
 
     def execute(self, cmd=""):
         """ shell execution, super dangerous """
@@ -66,12 +75,27 @@ class Tcomix(object):
         w, h = self.execute_w3m(w3m_getsize.format(filename=filename)).split()
         return int(w), int(h)
 
+    def get_screen_size(self):
+        w, h = self.execute("{} -test".format(W3MIMGDISPLAY)).split()
+        return int(w), int(h)
+
+    def get_font_dimensions(self):
+        w, h = self.get_screen_size()
+        w += 2
+        h += 2
+        return (w // curses.COLS), (h // curses.LINES)
+
     def draw(self, filename, x, y, w, h):
         return self.execute_w3m(w3m_display.format(filename=filename,
-                                                   x=x, y=y, w=w, h=h))
+                                                   x=int((x-0.2)*self.fontw),
+                                                   y=int(y*self.fonth),
+                                                   w=w, h=h))
 
     def clear(self, x, y, w, h):
-        return self.execute_w3m(w3m_clear.format(x=x, y=y, w=w, h=h))
+        return self.execute_w3m(w3m_clear.format(x=int((x-0.2)*self.fontw),
+                                                 y=int(y*self.fonth),
+                                                 w=int(w*1.01),
+                                                 h=int(h*1.01)))
 
     def imgfoo(self):
         return self.execute("ls ~/Pictures/*.jpg")
@@ -96,25 +120,31 @@ class Tcomix(object):
         while True:
             w, h = self.get_image_size(self.image_list[self.image_idx])
 
-            if w > self.MAXH:
+            if w > self.MAXW:
+                w, h = self._norm_h(w, h)
+
+            if h > self.MAXH:
                 w, h = self._norm_w(w, h)
 
-            self.draw(self.image_list[self.image_idx], 10, 10, w, h)
+            imgview_x, imgview_y = 21, 1
+            self.draw(self.image_list[self.image_idx],
+                      imgview_x, imgview_y, w, h)
 
             self.text(">" + " "*50)
             self.text("> {}".format(self.image_list[self.image_idx]))
-            self.win.refresh()
 
             c = self.win.getch()
             if c == ord('q'):
                 break
-            elif c == curses.KEY_UP:
+            elif c == curses.KEY_UP or c == ord('k'):
                 self.image_idx += 1
-            elif c == curses.KEY_DOWN:
+            elif c == curses.KEY_DOWN or c == ord('j'):
                 self.image_idx -= 1
 
             self.image_idx %= len(self.image_list)
-            self.clear(10, 10, w, h)
+            self.clear(imgview_x, imgview_y, w, h)
+            self.win.refresh()
+            self.filelist_box.refresh()
 
     def __del__(self):
         self.win.keypad(False)
